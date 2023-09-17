0376b7e073f346d42f19ca641fddc0e009b5e56e
SultikTel <sultiktel@gmail.com>
2023-09-18 01:29:20 +0600

    InitialCommit

---
+++ Test/Assets/Scripts/OnAndOff.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnAndOff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnOrOff()
+    {
+        if (gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+    public void Write()
+    {
+        Debug.Log("fd");
+    }
+
+    // Update is called once per frame
+
+}
